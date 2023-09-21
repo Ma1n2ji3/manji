a972dc7cfed7fba7f7bfc7269ae4721dde5a867a
manji <manjiamos023@gmail.com>
2023-09-21 10:31:49 +0100

    practice commit

--- today.py
+++ today.py
@@ -1,19 +1,35 @@
-trial = 'lets  see  now '
-new_trial = trial.strip()
-print(new_trial)
+# trial = 'lets  see  now '
+# new_trial = trial.strip()
+# print(new_trial)
 
-trial = 'manji is trying something new'
-new_trial = trial.title()
-print(new_trial)
+# trial = 'manji is trying something new'
+# new_trial = trial.title()
+# print(new_trial)
 
-string = ' i could eat bananas all day'
-new_string = string.partition()
-print(new_string)
+# string = ' i could eat bananas all day'
+# new_string = string.partition()
+# print(new_string)
 
-methods = ''
-new_methods = methods.isspace()
-print(new_methods)
+# methods = ''
+# new_methods = methods.isspace()
+# print(new_methods)
 
-links = ' we can only hope it works'
-new_link = links.isprintable()
-print(new_link)
+# links = ' we can only hope it works'
+# new_link = links.isprintable()
+# print(new_link)
+
+practice = 'is this manji'
+new_practice = practice.isupper()
+print(new_practice)
+
+manji = 'm\ta\tn\tj\ti'
+new_manji = manji.expandtabs()
+print(new_manji)
+
+edith = 'bestgirl'
+new_edith = edith.center(20)
+print(new_edith)
+
+Ibbi = ' his name is mosho'
+new_ibbi = Ibbi.encode()
+print(new_ibbi)
