abfdf293bc691674217b7f09c6d4e8db53e5ae2c
manji <manjiamos023@gmail.com>
2023-09-21 20:58:09 +0100

    madlibs commit

---
+++ madlibs.py
@@ -0,0 +1,27 @@
+name = input(' choose a name: ')
+
+p_name = input('choose a plural name: ')
+
+name2 = input('choose a name: ')
+
+location = input('name a location: ')
+
+description = input('çhoose a description: ')
+
+name3 = input('choose a name: ')
+
+print('------------------------')
+
+print('this story is about', name, ' who is learning about', p_name)
+
+print('she has a lecturer', name2, ',')
+
+print('she lives', 'in', location)
+
+print('her enthusiam is', description)
+
+print('and that is all thanks to', name3)
+
+print('Her boyfriend.')
+
+print('------------------------------')
